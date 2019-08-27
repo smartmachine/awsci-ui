3e5251b14df3818aac775bc1c8bce5f3fa75a1e5
Conrad Christensen <conrad.christensen@gmail.com>
2019-08-28 00:03:05 +0200

    authentication implemented

--- src/App.js
+++ src/App.js
@@ -1,26 +1,51 @@
 import React from 'react';
-import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import Button from "react-bootstrap/Button";
+import Container from "react-bootstrap/Container";
+import Jumbotron from "react-bootstrap/Jumbotron";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+    
+    constructor(props) {
+        super(props);
+        this.state = { githubURL: '' };
+    }
+    
+    componentDidMount() {
+
+        let host = "https://github.com/login/oauth/authorize?";
+
+        let queryData = {
+            client_id: "9ba972db1d356346f618",
+            redirect_uri: "https://awsci.io",
+            scope: "read:user repo",
+            allow_signup: "false"
+        };
+
+        this.setState({githubURL: host + App.encodeQueryData(queryData)});
+    }
+
+    static encodeQueryData(data) {
+        const ret = [];
+        for (let d in data)
+            ret.push(encodeURIComponent(d) + '=' + encodeURIComponent(data[d]));
+        return ret.join('&');
+    }
+
+    render() {
+        return (
+            <div>
+                <Container className="p-3">
+                    <Jumbotron>
+                        <h1 className="header">Welcome To AWSci</h1>
+                        <Button href={this.state.githubURL}>Login with Github</Button>
+                    </Jumbotron>
+                </Container>
+
+            </div>
+        );
+    }
 }
 
 export default App;
